ece500a44aa38782ceabec537c6bf8540e53b60f
Glenio Carvalho <gleniohenrique-carvalho@hotmail.com>
2019-09-23 15:05:13 -0300

    Request de imagens funcionando, porém não é  a best way;

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -9,7 +9,7 @@ import {ApiService} from '../../core/api.service';
 export class HomeComponent implements OnInit {
     nomes: any;
     imagens: any[] = [];
-    info: any[] = [];
+    info: any;
 
     constructor(private api: ApiService) {
     }
@@ -23,16 +23,11 @@ export class HomeComponent implements OnInit {
         setTimeout(() => {
             this.api.http('?offset=0&limit=50').subscribe(nomes => {
                 if (nomes.results !== undefined) {
-                    this.nomes = nomes.results;
+                    this.info = nomes.results;
                     //console.log(nomes.results);
                     for (let i = 0; i < nomes.results.length; i++) {
-                        this.info.push(nomes.results[i].name)
-                        console.log(this.info)
-                        this.api.http(`/${this.info[i]}`).subscribe(imagens => {
-                            setTimeout(() => {
-                                console.log(imagens.sprites.front_default);
-                                this.imagens[i] = imagens.sprites.front_default;
-                            });
+                        this.api.http(`/${nomes.results[i].name}`).subscribe(imagens => {
+                            this.imagens[i] = imagens.sprites.front_default;
                         });
                     }
                 }
