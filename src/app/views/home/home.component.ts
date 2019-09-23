9a042f4a5b98c1bb566cd3da98cdbb29f08d5f5e
Glenio Carvalho <gleniohenrique-carvalho@hotmail.com>
2019-09-23 17:47:01 -0300

    Falta fazer request de habilidades

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ApiService} from '../../core/api.service';
 
 @Component({
@@ -11,38 +11,49 @@ export class HomeComponent implements OnInit {
     imagens: any[] = [];
     info: any;
 
+    @ViewChild('myBtn', {static: false}) myBtn: ElementRef;
+    @ViewChild('myModal', {static: false}) myModal: ElementRef;
+    @ViewChild('close', {static: false}) close: ElementRef;
+
     constructor(private api: ApiService) {
     }
 
     ngOnInit(): void {
         this.getNamePokemon();
-        //this.getImagesPokemon();
     }
+
+    openModal() {
+        setTimeout(() => {
+            this.myModal.nativeElement.style.display = 'block';
+            for(let i = 0; i < this.nomes.length; i++) {
+                this.api.http(`/${this.nomes[i].name}`).subscribe(data => {
+                    console.log(data);
+                })
+            }
+        });
+    }
+
+    closeModal() {
+        setTimeout(() => {
+            this.myModal.nativeElement.style.display = 'none';
+        })
+    }
+
 
     getNamePokemon() {
         setTimeout(() => {
             this.api.http('?offset=0&limit=50').subscribe(nomes => {
                 if (nomes.results !== undefined) {
-                    this.info = nomes.results;
-                    //console.log(nomes.results);
+                    this.nomes = nomes.results;
                     for (let i = 0; i < nomes.results.length; i++) {
-                        this.api.http(`/${nomes.results[i].name}`).subscribe(imagens => {
-                            this.imagens[i] = imagens.sprites.front_default;
+                        this.api.http(`/${nomes.results[i].name}`).subscribe(data => {
+                            this.imagens[i] = data.sprites.front_default;
                         });
                     }
                 }
             });
         });
 
-    }
-
-    getImagesPokemon() {
-        this.api.http(`/25`).subscribe(imagens => {
-            setTimeout(() => {
-                this.imagens = imagens.sprites.front_default;
-                console.log(imagens);
-            });
-        });
     }
 
 
